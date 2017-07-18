aa453611b80e3b7090f27f43f09c1ebfe2c8fd83
Qian Li <arthur.liqian@gmail.com>
2017-07-18 11:36:19 +0800

    add comment

--- _drafts/rabbitmq/sample_code/fanout_exchange/receive.py
+++ _drafts/rabbitmq/sample_code/fanout_exchange/receive.py
@@ -8,6 +8,10 @@ connection = BlockingConnection(ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange=exchange_name, type='fanout')
+# when no queue name is given, a random unique name will be generated. The 
+# fanout exchange always copies and dispatches each message to all of the 
+# bound queues. In order to demonstrate that behavior, each receiver process
+# requires a separated queue, so we leverage the automitcally generated queues.
 queue_declaring_result = channel.queue_declare()
 queue_name = queue_declaring_result.method.queue
 
